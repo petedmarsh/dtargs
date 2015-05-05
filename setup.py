03edd89abc13b35d2fb24cb7ba14ff32c56f09f7
Peter Marsh <pete.d.marsh@gmail.com>
2015-05-05 15:04:37 +0100

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='dtargs',
+      version='0.1.0',
+      description='Date/time types for Argparse',
+      author='Peter Marsh',
+      author_email='pete.d.marsh@gmail.com',
+      py_modules=['dtargs'])
