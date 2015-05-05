06f91127bd7bfa3fc8ca2d19731ee036b26ed544
Peter Marsh <pete.d.marsh@gmail.com>
2015-05-05 17:13:19 +0100

    Implement dtargs.DateTimeType
    This is a factory for creating datetime.date instances from strings.
    The class can be passed a format string in it's constructor, this can
    be anything datime.strptime accepts - if a custom format is not
    specified then it will default to '%Y-%m-%dT%H:%M:%SZ' which an ISO8061
    compliant date/time format (with UTC timezone).

--- dtargs.py
+++ dtargs.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import pytz
 from datetime import datetime
 
 
@@ -27,3 +28,56 @@ class DateType(object):
 
     def __repr__(self):
         return '%s(date_format="%s")' % (type(self).__name__, self.date_format)
+
+
+class DateTimeType(object):
+    """Factory for creating datetime.datetime object types
+
+    Instances of DateTimeType are typically passed as type= arguments to the
+    ArgumentParser add_argument() method.
+
+    The tz parameter is handled thusly:
+
+        1. If date_time_format does not include timezone directives and tz is
+           None then a naive datetime instance will be returned (i.e.
+           tzinfo=None)
+        2. If date_time_format does include timezone directives and ts is None
+           then a timezone-aware datetime instance will be returned with
+           whatever tzinfo is set by strptime (i.e. tzinfo=datetime.timezone)
+        3. If date_time_format does not include timezone directives and tz is
+           not None then a timezone-aware datetime instance will be returned
+           with tzinfo set to tz
+        4. If date_time_format does include timezone directives and ts is not
+           None then the datetime instance from strptime will be converted into
+           the tz timezone
+
+    :param date_time_format: A date/time format string that datetime.strptime
+                             accepts (default '%Y-%m-%dT%H:%M:%SZ')
+    :type date_time_format: str
+    :param tz: The timezone to apply to the date/time, may be None in which
+               case a naive datetime.datetime instance is returned (default
+               pytz.utc)
+    :type tz: datetime.timezone
+    """
+
+    def __init__(self, date_time_format='%Y-%m-%dT%H:%M:%SZ', tz=pytz.utc):
+        self.date_time_format = date_time_format
+        self.tz = tz
+
+    def __call__(self, string):
+        try:
+            date_time = datetime.strptime(string, self.date_time_format)
+            if self.tz:
+                if date_time.tzinfo:
+                    date_time = date_time.astimezone(self.tz)
+                else:
+                    date_time = date_time.replace(tzinfo=self.tz)
+            return date_time
+        except (TypeError, ValueError) as e:
+            raise  argparse.ArgumentTypeError('could not parse "%s" as "%s"'
+                % (string, self.date_time_format))
+
+    def __repr__(self):
+        return '%s(date_time_format="%s", tz=%s)' % (type(self).__name__,
+                                                      self.date_time_format,
+                                                      repr(self.tz))
--- test_dtargs.py
+++ test_dtargs.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from datetime import date
+from datetime import date, datetime
 import argparse
 import dtargs
+import mock
 import pytest
+import pytz
 
 
 class TestDateType:
@@ -45,3 +47,104 @@ class TestDateType:
             dt = dtargs.DateType('%d/%m/%Y')
             date_string = '2015-01-02'
             dt(date_string)
+
+
+class TestDateTime:
+
+    def test_no_format_specified(self):
+        assert dtargs.DateTimeType().date_time_format == '%Y-%m-%dT%H:%M:%SZ'
+
+    def test_no_timezone_specified(self):
+        assert dtargs.DateTimeType().tz == pytz.utc
+
+    def test_repr(self):
+        dtt = dtargs.DateTimeType()
+        assert repr(dtt) == 'DateTimeType(date_time_format="%s", tz=%s)' % (
+            dtt.date_time_format, repr(dtt.tz))
+
+    def test_default_format_call_with_correctly_formatted_date_time(self):
+        dtt = dtargs.DateTimeType()
+        date_string = '2015-01-02T12:34:56Z'
+        assert dtt(date_string) == datetime(2015, 1, 2, 12, 34, 56,
+                                            tzinfo=pytz.utc)
+
+    def test_default_format_tz_none_with_correctly_formatted_date_time(self):
+        dtt = dtargs.DateTimeType(tz=None)
+        date_string = '2015-01-02T12:34:56Z'
+        assert dtt(date_string) == datetime(2015, 1, 2, 12, 34, 56)
+
+    def test_default_format_custom_tz_with_correctly_formatted_date_time(
+            self):
+        est = pytz.timezone('EST')
+        dtt = dtargs.DateTimeType(tz=est)
+        date_string = '2015-01-02T12:34:56Z'
+        assert dtt(date_string) == datetime(2015, 1, 2, 12, 34, 56, tzinfo=est)
+
+    def test_default_format_call_with_incorrectly_formatted_date_time(self):
+        date_string = 'Z 12:34:56 02/01/2015'
+        with pytest.raises(argparse.ArgumentTypeError):
+            dtt = dtargs.DateTimeType()
+            dtt(date_string)
+
+    def test_call_with_none(self):
+        with pytest.raises(argparse.ArgumentTypeError):
+            dtt = dtargs.DateTimeType()
+            dtt(None)
+
+    def test_custom_format_call_with_correctly_formatted_date_time(self):
+        dtt = dtargs.DateTimeType('%H:%M:%S_%d/%m/%Y')
+        date_string = '12:34:56_01/02/2015'
+        assert dtt(date_string) == datetime(2015, 2, 1, 12, 34, 56,
+                                            tzinfo=pytz.utc)
+
+    def test_custom_format_call_with_incorrectly_formatted_date_time(self):
+        date_string = '2015-01-02T12:34:56Z'
+        with pytest.raises(argparse.ArgumentTypeError):
+            dtt = dtargs.DateTimeType('%H:%M:%S_%d/%m/%Y')
+            dtt(date_string)
+
+    def test_custom_format_without_offset_tz_is_none_call_with_valid_date(
+            self):
+        date_time_format = '%H:%M:%S_%Y-%m-%d'
+        date_string = '12:34:56_2015-02-01'
+        dtt = dtargs.DateTimeType(date_time_format, tz=None)
+        assert dtt(date_string) == datetime(2015, 2, 1, 12, 34, 56,
+                                            tzinfo=None)
+
+    def test_custom_format_without_offset_custom_tz_call_with_valid_dt(self):
+        est = pytz.timezone('EST')
+        dtt = dtargs.DateTimeType('%H:%M:%S_%d/%m/%Y', tz=est)
+        date_string = '12:34:56_01/02/2015'
+        assert dtt(date_string) == datetime(2015, 2, 1, 12, 34, 56, tzinfo=est)
+
+    @mock.patch('dtargs.datetime')
+    def test_custom_format_with_offset_custom_tz_call_with_valid_dt(self,
+                                                                    mock_dt):
+        date_time_format = 'parsing mocked, format irrelevant'
+        date_string = 'parsing mocked, string irrelevant '
+        est = pytz.timezone('EST')
+
+        mock_parsed_date_time = datetime(2015, 1, 2, 12, 34, 56,
+                                         tzinfo=pytz.utc)
+        mock_dt.strptime.return_value = mock_parsed_date_time
+
+        dtt = dtargs.DateTimeType(date_time_format, est)
+        assert dtt(date_string) == datetime(2015, 1, 2, 7, 34, 56,
+                                            tzinfo=est)
+        mock_dt.strptime.assert_called_with(date_string, date_time_format)
+
+
+    @mock.patch('dtargs.datetime')
+    def test_custom_format_with_offset_tz_none_call_with_valid_dt(self,
+                                                                  mock_dt):
+        date_time_format = 'parsing mocked, format irrelevant'
+        date_string = 'parsing mocked, string irrelevant '
+        est = pytz.timezone('EST')
+
+        mock_parsed_date_time = datetime(2015, 1, 2, 12, 34, 56, tzinfo=est)
+        mock_dt.strptime.return_value = mock_parsed_date_time
+
+        dtt = dtargs.DateTimeType(date_time_format, None)
+        assert dtt(date_string) == datetime(2015, 1, 2, 12, 34, 56,
+                                            tzinfo=est)
+        mock_dt.strptime.assert_called_with(date_string, date_time_format)
