a9918d87dc74f5ac67e76dd5e359ab4aa46d0052
Peter Marsh <pete.d.marsh@gmail.com>
2015-05-05 22:29:25 +0100

    Add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,19 @@ from distutils.core import setup
 
 setup(author='Peter Marsh',
       author_email='pete.d.marsh@gmail.com',
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Libraries',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4'
+      ],
       description='Date/time types for Argparse',
       install_requires=['pytz'],
       license='MIT',
